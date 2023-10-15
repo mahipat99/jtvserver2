22c6d187c5dc23a2bd59b587c8e809b3af285cd9
Mahipat Chauhan <mahipat5678@gmail.com>
2023-10-15 11:47:44 +0530

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -22,7 +22,8 @@ NC='\033[0m' # No Color
 # Function to get device IP address
 get_device_ip() {
     ip_address=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-    echo "$ip_address"}
+    echo "$ip_address"
+}
 
 # Function to install dependencies and setup JTVServer
 install() {
